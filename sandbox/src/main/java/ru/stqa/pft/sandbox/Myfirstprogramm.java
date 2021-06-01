0c75c32187e8fbc82ea911073160ad41bd943866
KateZiankovich <Bhaktinkatya@gmail.com>
2021-06-01 11:12:35 +0300

    д.з. Пункты 1-3

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Myfirstprogramm.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Myfirstprogramm.java
@@ -12,10 +12,19 @@ public class Myfirstprogramm {
 
     Rectangle r = new Rectangle(4, 6);
     System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+
+    Point p1 = new Point(1, 3);
+    Point p2 = new Point(3,5);
+    double d = distance(p1, p2);
+    System.out.println("Расстоание между точками " + "X" + " и " + "Y " + " = " + distance(p1, p2));
   }
   public static void hello(String somebody) {
     System.out.println("Hello," + somebody +"!");
   }
 
+  public static double distance(Point p1, Point p2) {
+    return Math.sqrt(((p1.y - p1.x) + (p2.y -p2.x) ));
+
+  }
 
 }
