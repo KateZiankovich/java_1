d9c626c586cbf440914a50c39122a112b1dae437
KateZiankovich <Bhaktinkatya@gmail.com>
2021-06-03 09:25:21 +0300

    Задание 3

--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -6,8 +6,8 @@ import org.testng.annotations.Test;
 public class PointTests {
   @Test
   public void testDistance() {
-    Point p1 = new Point(5,7);
-    Point p2 = new Point(5, 7);
+    Point p1 = new Point(1,1);
+    Point p2 = new Point(3, 5);
     Assert.assertEquals(p1.distance(p2), 2.0);
 
   }
